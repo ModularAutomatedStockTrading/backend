239f94c950560c1ec6026d581ce976300290daf2
williamBMDK <williambillemeyling@gmail.com>
2020-03-14 02:11:57 +0100

    delete models

--- src/server/routes/models.js
+++ src/server/routes/models.js
@@ -18,4 +18,9 @@ router.patch("/:id", async (req, res) => {
     res.send({model});
 });
 
+router.delete("/:id", async (req, res) => {
+    await Model.findByIdAndDelete(req.params.id);
+    res.sendStatus(200);
+});
+
 module.exports = router;
