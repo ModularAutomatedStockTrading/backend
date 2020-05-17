b9bd41cd831eb7b5e173f784ec7e86887155fd2a
Asmus <asmus.torsleff@gmail.com>
2020-05-17 21:32:06 +0200

    documentation
    documenting neuralNetwork.cpp and activiationFunctions.cpp

--- src/nn-training/evolutionary/src/activationfunctions.cpp
+++ src/nn-training/evolutionary/src/activationfunctions.cpp
@@ -6,46 +6,84 @@
 #define ACTIVATION_INCLUDED
 
 namespace Activation{
-
+	/*
+	'Activates' an input, a, via the identity activation function and returns the output.
+	(this activation function doesn't do anything by design)
+	Input -> double a
+	Output -> double a
+	*/
 	double identityActivate(double a) {
 		return a;
 	}
-
+	/*
+	'Activates' an input, a, via the ReLU activation function and returns the output.
+	Input -> double a
+	Output -> double a
+	*/
 	double ReLUActivate(double a) {
 		if (a <= 0) { return 0; }
 		return a;
 	}
-
+	/*
+	'Activates' an input, a, via the binary step activation function and returns the output.
+	Input -> double a
+	Output -> double a
+	*/
 	double binaryStepActivate(double a) {
 		if (a < 0) { return 0; }
 		return 1;
 	}
-
+	/*
+	'Activates' an input, a, via the logistic activation function and returns the output.
+	Input -> double a
+	Output -> double a
+	*/
 	double logisticActivate(double a) {
 		return 1/(1 + exp(-a));
 	}
-
+	/*
+	'Activates' an input, a, via the tanH activation function and returns the output.
+	Input -> double a
+	Output -> double a
+	*/
 	double tanHActivate(double a) {
 		return (exp(a) - exp(-a))/(exp(a) + exp(-a));
 	}
-
+	/*
+	'Activates' an input, a, via the SQNL activation function and returns the output.
+	Input -> double a
+	Output -> double a
+	*/
 	double SQNLActivate(double a) {
 		if (a < -2) { return -1; }
 		if (a < 0) { return a + (a*a)/4; }
 		if (a <= 2) { return a - (a*a)/4; }
 		return 1;
 	}
-
+	/*
+	'Activates' an input, a, via the soft sign activation function and returns the output.
+	Input -> double a
+	Output -> double a
+	*/
 	double softsignActivate(double a) {
 		return a/(1 + abs(a));
 	}
-
+	/*
+	'Activates' an input, a, via the ISRU activation function and returns the output.
+	Input -> double a
+	Output -> double a
+	*/
 	double ISRUActivate(double a) {
-		int b = 1;
-		return a/sqrt(1.0 + b*a*a); //retuns value in range [-1/sqrt(b),1/sqrt(b)]
+		double b = 1; // Must be larger than 0
+		return a/sqrt(1.0 + b*a*a); //Retuns value in range [-1/sqrt(b),1/sqrt(b)]
 	}
-
-	double activate(double a, std::string activationType) {
+	/*
+	Calls an activation function specified in the input. 
+	Input -> double a, string activationType.
+	Output -> double a
+	
+	*/
+	double activate(double a, std::string activationType) { 
 		if 		(activationType == "identity")   return identityActivate(a);
 		else if (activationType == "ReLU")	     return ReLUActivate(a);
 		else if (activationType == "binaryStep") return binaryStepActivate(a);
--- src/nn-training/evolutionary/src/neuralNetwork.cpp
+++ src/nn-training/evolutionary/src/neuralNetwork.cpp
@@ -17,10 +17,16 @@ void NeuralNetwork::set_NN(std::vector<std::vector<std::vector<double>>>& NN) {
     this->NN = NN;
 }
 
+
 std::vector<double> NeuralNetwork::matrixMultiplication(
     std::vector<double>& a,
     std::vector<std::vector<double>> b
 ) {
+	/*
+	Multiplies a matrix and a vector.
+	Input -> vector<std::vector<double>> b (the matrix), vector<double>& a (the vector)
+	Output -> std::vector<double> res (a vector)
+	*/
     std::vector<double> res(b[0].size(), 0);
     for (int i = 0; (unsigned)i < res.size(); i++) {
         for (int j = 0; (unsigned)j < (withBias ? b.size() - 1 : b.size()); j++)
@@ -34,6 +40,11 @@ std::vector<double> NeuralNetwork::matrixMultiplication(
 // b[node][nextLayerNode] = weight;
 
 void NeuralNetwork::predict(std::vector<double>& input, std::vector<double>& output) {
+	/*
+	Predicts an output via the the neural network NN, changes the vector output to reflect this.
+	Input -> vector<double>& input, vector<double>& output
+	Output -> void
+	*/
 	std::vector<double> current = input;
 	for (int layer = 0; (unsigned)layer < NN.size() - 1; layer++) {
 		current = matrixMultiplication(current, NN[layer]);
@@ -47,6 +58,11 @@ void NeuralNetwork::predict(std::vector<double>& input, std::vector<double>& out
 }
 
 void NeuralNetwork::modifyWeights(double mutationRange) {
+	/*
+	Modifies all weights, within some range, in the neural network NN.
+	Input -> double mutationRange
+	Output -> void
+	*/
     double range = (double)(rand() % (int)(mutationRange * 1000000)) / 1000000; 0 - 0.5;
 	for (int i = 0; (unsigned)i < NN.size(); i++) {
         for (int j = 0; (unsigned)j < NN[i].size(); j++) {
@@ -60,6 +76,12 @@ void NeuralNetwork::modifyWeights(double mutationRange) {
 }
 
 void NeuralNetwork::generateRandomInstance(std::vector<int>& modelTemplate) {
+	/*
+	Resizes the 3D vector NN to match the modelTemplate 3D vector.
+	Sets all the weights in the neural network NN to a random value in the range [-0.5,05].
+	Input -> double vector<int>& modelTemplate
+	Output -> void
+	*/
     NN.resize(modelTemplate.size());
     for (int i = 0; (unsigned)i < modelTemplate.size() - 1; i++) {
         NN[i].resize(modelTemplate[i]);
@@ -71,34 +93,3 @@ void NeuralNetwork::generateRandomInstance(std::vector<int>& modelTemplate) {
         }
     }
 }
-
-/*
-int main()
-{
-	std::srand(time(NULL));
-	std::vector<int> nodesInLayers(3, 0);
-	nodesInLayers[0] = 4;
-	nodesInLayers[1] = 2;
-	nodesInLayers[2] = 1;
-	std::vector<std::vector<std::vector<double>>> NN = generateRandomInstance(3, nodesInLayers);
-	std::vector<std::vector<double>> input(1, std::vector<double>(4, 0.0));
-	input[0][0] = 1;
-	input[0][1] = 2;
-	input[0][2] = 3;
-	input[0][3] = 4;
-
-    //set NN to test NN = [[[1.1, 1.2], [1.3, 0.2], [1.2, 1.2], [1.3, 3.4]], [[0.9], [1.0]]] remember to set nodesInLayers = [4, 2, 1], predict should be 30.05 with input [[1, 2, 3, 4]]
-	//int l = 0;
-	//double n[] = { 1.1, 1.2, 1.3, 0.2, 1.2, 1.2, 1.3, 3.4, 0.9, 1.0 };
-	//for (int i = 0; i < NN.size(); i++) {
-	//	for (int j = 0; j < NN[i].size(); j++) {
-	//		for (int k = 0; k < NN[i][j].size(); k++) {
-	//			NN[i][j][k] = n[l];
-	//			l++;
-	//		}
-	//	}
-	//}
-
-	std::vector<std::vector<double>> prediction = predict(input, NN);
-}
-*/
